804d97c2fba90dc1a0176ea8d606a21640e38846
Jossy22 <josephineokaforadi@gmail.com>
2022-12-10 10:13:29 -0800

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
+* main - Entry
 *
-*main - Entry
-*
-*Return: Always 0
+* Return: Always 0
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle);
+	puts("\"Programming is like building a multi lingual puzzle");
 	return (0);
-}#*
+}
