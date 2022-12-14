c2e3583d39e8606bf3955b20e6ca15e6837a3355
Jossy22 <josephineokaforadi@gmail.com>
2022-12-13 21:25:20 -0800

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,12 @@
+ifindef MAIN_H
+#define MAIN_H
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sigh(int n);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
@@ -13,4 +16,3 @@ void print_to_98(int n);
 void num(int r, int n);
 
 #endif
-
