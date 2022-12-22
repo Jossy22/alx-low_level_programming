e7fe8cf95e8b87920a4d068ca65bfb6780cbb999
Jossy22 <josephineokaforadi@gmail.com>
2022-12-22 05:42:54 -0800

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,19 +5,17 @@
  * @n: integer params
  * Return: 0
  */
-
 void rev_string(char *n)
 {
 	int i = 0;
 	int j = 0;
 	char temp;
-	
+
 	while (*(n + i) != '\0')
 	{
 		i++;
 	}
 	i--;
-
 	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
